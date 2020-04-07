01c15419ac62eb889623f78da42917b65b1080a8
Piotr <piotrekert90@gmail.com>
2020-04-07 08:30:30 +0200

    Add `Http` prefix for better readability. Rename `model` package to `models`.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:climaweatherapp/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as Http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -25,7 +25,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Response response = await get(
+    Http.Response response = await Http.get(
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
 
     if (response.statusCode == 200) {
