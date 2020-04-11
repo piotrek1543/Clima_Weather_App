1f00e50d40edbefe1e53e5aaa4aa238639c786e3
Piotr <piotrekert90@gmail.com>
2020-04-11 20:02:57 +0200

    Remove disused imports and unnecessary comments.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:climaweatherapp/screens/city_screen.dart';
 import 'package:climaweatherapp/screens/loading_screen.dart';
-import 'package:climaweatherapp/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,24 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: WeatherPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(home: WeatherPage());
 }
 
 class WeatherPage extends StatelessWidget {
